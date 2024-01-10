302a397075c2c9bb5608fbea6905dece78bdfaf4
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2024-01-10 15:48:42 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -2,7 +2,7 @@ import speech_recognition as sr
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-from easygui import *
+from easygui import * 
 import os
 from PIL import Image, ImageTk
 from itertools import count
